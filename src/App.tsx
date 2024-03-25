adf9d1c0762766b77a744af4bc8391b93464fd08
정호원 <jjhw0106@naver.com>
2024-03-25 17:08:29 +0900

    Feat: 리액트 강의 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+function Header(props: any) {
+  return <header>
+    <h1><a href='/'>{props.title}</a> </h1>
+  </header>
+}
+
+function Nav(props: Array<any>) {
+  return <nav>
+    <ol>
+      {
+        props.map((prop)=>(
+          <li>{prop}</li>
+        ))
+      }
+    </ol>
+  </nav>
+}
+
+function Article(props: any) {
+  return <article>
+    <h2>{props.title}</h2>
+    {props.body}
+  </article>
+}
+
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is...'},
+    {id:2, title:'css', body:'css is...'},
+    {id:3, title:'script', body:'script is...'},
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="REACT"></Header>
+      <Nav contents={topics}></Nav>
+      <Article title="Welcome" body="Hello,Web"></Article>
     </div>
   );
 }

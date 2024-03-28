9de3e770b4c069980600085a9896cf1a8a6cefcd
정호원 <jjhw0106@naver.com>
2024-03-29 00:09:48 +0900

    Feat: 생활코딩 리액트 강의 업데이트

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,32 @@ import './App.css';
 
 function Header(props: any) {
   return <header>
-    <h1><a href='/'>{props.title}</a> </h1>
+    <h1><a href='/' onClick={function(event) {
+      event.preventDefault();
+      props.onChangeMode();
+    }}>{props.title}</a> </h1>
   </header>
 }
 
-function Nav(props: Array<any>) {
+export interface Topic {
+  id: number
+  title: string
+  body: string
+}
+interface TopicProps {
+  topics: Topic[],
+  thre: string,
+  onChangedMode: (id: number) => void;
+}
+function Nav({topics, thre: sentence}: TopicProps) {
   return <nav>
+    <p>{sentence}</p>
     <ol>
       {
-        props.map((prop)=>(
-          <li>{prop}</li>
+        topics.map((prop)=>(
+          <li><a href='/' onClick={()=>alert(prop.id)}>{prop.id}</a></li>
         ))
-      }
+      } 
     </ol>
   </nav>
 }
@@ -33,10 +47,16 @@ function App() {
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'script', body:'script is...'},
   ]
+
   return (
     <div>
-      <Header title="REACT"></Header>
-      <Nav contents={topics}></Nav>
+      <Header title="REACT" onChangeMode={
+        ()=>alert("Header!!")
+      }
+      ></Header>
+      <Nav topics={topics} thre="ffdf" onChangedMode={(id)=>{
+        alert(id);
+      }}></Nav>
       <Article title="Welcome" body="Hello,Web"></Article>
     </div>
   );

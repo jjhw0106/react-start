eaed39686d858def0c7db6da6d693695538e121d
정호원 <jjhw0106@naver.com>
2024-03-31 15:37:55 +0900

    Feat: 생활코딩 리액트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,7 +6,7 @@ function Header(props: any) {
   return <header>
     <h1><a href='/' onClick={function(event) {
       event.preventDefault();
-      props.onChangeMode();
+      props.onChangedMode();
     }}>{props.title}</a> </h1>
   </header>
 }
@@ -21,13 +21,16 @@ interface TopicProps {
   thre: string,
   onChangedMode: (id: number) => void;
 }
-function Nav({topics, thre: sentence}: TopicProps) {
+function Nav({topics, thre: sentence, onChangedMode}: TopicProps) {
   return <nav>
     <p>{sentence}</p>
     <ol>
       {
         topics.map((prop)=>(
-          <li><a href='/' onClick={()=>alert(prop.id)}>{prop.id}</a></li>
+          <li><a href='/' onClick={(event)=>{
+            event.preventDefault();
+            onChangedMode(prop.id)
+          }}>{prop.id}</a></li>
         ))
       } 
     </ol>
@@ -41,23 +44,48 @@ function Article(props: any) {
   </article>
 }
 
+function Create() {
+  return <form>
+    <p><input></input></p>
+    <p><textarea></textarea></p>
+    <p><input type='submit'></input></p>
+  </form>
+}
+
 function App() {
+  /* _mode[0]은 초깃값, mode[1]은 값을 변경시키는 함수 */
+  // const _mode = useState('WELCOME');
+  // const mode = _mode[0];
+  // const setMode = _mode[1];
+  // 축약형
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState<null|number>(null);
   const topics = [
     {id:1, title:'html', body:'html is...'},
     {id:2, title:'css', body:'css is...'},
     {id:3, title:'script', body:'script is...'},
-  ]
+  ];
+  const topic = topics.find(topic => topic.id === id);
 
-  return (
-    <div>
-      <Header title="REACT" onChangeMode={
-        ()=>alert("Header!!")
-      }
-      ></Header>
-      <Nav topics={topics} thre="ffdf" onChangedMode={(id)=>{
-        alert(id);
+  let content = null;
+  if (mode === 'WELCOME') {
+      content = <Article title = "Welcome" body="Hello, WEB"></Article>
+    } else if (mode === 'READ') {
+      content = <Article title={topic?.title} body="Hello, Read"></Article>
+    }
+    
+    return (
+      <div>
+      <Header title="REACT" onChangedMode={()=>{
+        setMode("WELCOME");
+      }}>
+      </Header>
+      <Nav topics={topics} thre="ffdf" onChangedMode={(_id)=>{
+        setMode('READ');
+        setId(_id);
       }}></Nav>
-      <Article title="Welcome" body="Hello,Web"></Article>
+      {content}
+      <Create></Create>
     </div>
   );
 }
